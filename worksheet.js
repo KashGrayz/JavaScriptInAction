7e462f21900597b8bade5621687a4ea17634179b
Kashar Grays <kash.different@Kashars-MacBook-Pro.local>
2022-11-23 14:26:16 -0500

    Variable Tasks Completed

--- worksheet.js
+++ worksheet.js
@@ -2,4 +2,25 @@
 // console.log('Kash');
 // console.log(true);
 
-// TASK 1
+            //-VARIABLES-\\    
+
+  //TASK 1\\ 
+
+// let dayOfWeek = 'Monday'
+// let console.log(dayOfWeek)
+
+// let dayOfWeek = 'Friday'
+// console.log(`I can't wait for ${dayOfWeek}!`)
+
+  //TASK 2\\ 
+
+// let animalInput = prompt("What is your favorite animal?")
+// let colorInput = prompt("What is your favorite color?")
+// console.log(`I've never seen a ${colorInput} ${animalInput}!`)
+
+            //-CONDITIONALS-\\
+            
+  //TASK 1\\
+
+
+
