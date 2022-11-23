6a85ebb15d6c7aa91a24682af570270b4ca173c7
Kashar Grays <kash.different@Kashars-MacBook-Pro.local>
2022-11-23 15:12:01 -0500

    Random Numbers Tasks Completed

--- worksheet.js
+++ worksheet.js
@@ -2,9 +2,10 @@
 // console.log('Kash');
 // console.log(true);
 
+
             //-VARIABLES-\\    
 
-  //TASK 1\\ 
+  //TASK 1 : Days of the week\\ 
 
 // let dayOfWeek = 'Monday'
 // let console.log(dayOfWeek)
@@ -12,15 +13,52 @@
 // let dayOfWeek = 'Friday'
 // console.log(`I can't wait for ${dayOfWeek}!`)
 
-  //TASK 2\\ 
+  //TASK 2 : User Imput\\ 
 
 // let animalInput = prompt("What is your favorite animal?")
 // let colorInput = prompt("What is your favorite color?")
 // console.log(`I've never seen a ${colorInput} ${animalInput}!`)
 
+
             //-CONDITIONALS-\\
-            
-  //TASK 1\\
+
+  //TASK 1 : Meals\\
+
+// let timeOfDay = 900;
+
+// let bestMeal = 'Eggy Cheese Ramen';
+
+// if(timeOfDay < 1200) {
+//     console.log(bestMeal);
+// }
+// else if(timeOfDay > 1200 && timeOfDay < 1700) {
+//     console.log(bestMeal);
+// }
+// else if(timeOfDay > 1700) {
+//     console.log(bestMeal);
+// }
+
+    //TASK 2 : Random Number\\
+
+// let randNum = Math.floor(Math.random() * 11);
+
+// if(randNum >= 0 && randNum <= 2) {
+//     console.log("Beatles");
+// }
+// else if(randNum >= 3 && randNum <= 5) {
+//     console.log("Stones");
+// }
+// else if(randNum >= 6 && randNum <= 8) {
+//     console.log("Floyd");
+// }
+// else if(randNum == 9 || randNum == 10) {
+//     console.log("Hendrix");
+// }
+
+            //-"FOR" LOOPS-\\
+
+    //TASK 1 :\\
+
 
 
 
